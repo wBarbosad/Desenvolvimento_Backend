3a209c7432eaf96b53dac0165e98498ba2c34b73
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2023-07-29 08:58:41 -0300

    Lista 6

--- Listas/Lista_4/cosmeticoAvon/cosmeticoAvon/src/main/java/br/com/avon/cosmeticoAvon/controllers/CosmeticoController.java
+++ Listas/Lista_4/cosmeticoAvon/cosmeticoAvon/src/main/java/br/com/avon/cosmeticoAvon/controllers/CosmeticoController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("cosmeticos")
+@RequestMapping("/cosmeticos")
+@CrossOrigin("*")
 public class CosmeticoController {
 
     @Autowired
